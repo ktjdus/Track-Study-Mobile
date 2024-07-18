d8910c9cd32db4e330c3f5c51c1526c6d6937669
ktjdus <3217sy@khu.ac.kr>
2024-07-18 22:02:28 +0900

    Update Future1.dart

--- Future1.dart
+++ Future1.dart
@@ -1,25 +1,24 @@
-Map login() {
-  return { 'myId': 'myToken'};
+Future<void> login() {
+  return Future.delayed(Duration(seconds: 2)).then((value) {
+    print({'myID': 'myToken'});
+  });
 }
-  
-Map profile() {
-  return { 'myId': 'myName'};
+
+Future<void> profile() {
+  return Future.delayed(Duration(seconds: 1)).then((value) {
+    print({ "myID": "myName"});
+  });
 }
-  
-List notice() {
-  return ['First', 'second', 'third'];
+
+Future<void> notice() {
+  return Future.delayed(Duration(milliseconds: 1500)).then((value) {
+    print(['First', 'Second', 'Third']);
+  });
 }
 
 void main() {
-  Future.delayed(Duration(milliseconds: 2000)).then((value) {
-    print(login());
-    
-    Future.delayed(Duration(milliseconds: 1000)).then((value) {
-      print(profile());
-    });
-    
-    Future.delayed(Duration(milliseconds: 1500)).then((value) {
-      print(notice());
-    });
+  Future.any([login()]).then((value) {
+    profile();
+    notice();
   });
 }
